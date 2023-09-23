dde35732f3f654e35f29f75a01627d1427dea3fb
devpool <mr.khahani@gmail.com>
2023-09-23 14:17:24 -0400

    chore: [MRKH-0017] add demo and prod flavor for core network

---
+++ Crane/core/network/src/prod/kotlin/androidx.compose.samples.core.network.di/FlavoredNetworkModule.kt
@@ -0,0 +1,16 @@
+package androidx.compose.samples.core.network.di
+
+import androidx.compose.samples.core.network.FsqNetworkDataSource
+import androidx.compose.samples.core.network.retrofit.RetrofitFsqNetwork
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface FlavoredNetworkModule {
+
+    @Binds
+    fun RetrofitFsqNetwork.binds(): FsqNetworkDataSource
+}
